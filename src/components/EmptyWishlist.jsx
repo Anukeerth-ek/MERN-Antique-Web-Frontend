c71e79df1830f7187a4bc656e3ea6b0919d7f64b
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-07-25 11:21:14 +0530

    Wraped up empty component:

--- src/components/EmptyWishlist.jsx
+++ src/components/EmptyWishlist.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { GiShoppingCart } from "react-icons/gi";
-import ArtCard from './ArtCard';
+import ArtCard from "./ArtCard";
+import { Link } from "react-router-dom";
 const EmptyWishlist = () => {
-  return (
-    <div>
-        <div className='flex items-center flex-col justify-center '>
-            <img src='https://static.vecteezy.com/system/resources/thumbnails/017/745/125/small_2x/empty-shopping-cart-side-view-for-designing-various-shopping-promotions-png.png' alt='cart-image'
-             className=' w-20 h-30'
-            />
-            <p>No items yet? Continue shopping to explore more.</p>
-            <button>Sign in</button>
-            <button>Explore items</button>
-        </div>
-       
-    </div>
-  )
-}
 
-export default EmptyWishlist
+    const[arts, setArts] = useState([])
+
+    useEffect(()=> {
+        fetch('https://antique-web.onrender.com/all-arts').then(res=> res.json()).then(data => setArts(data.slice(14, 18)))
+    }, [])
+
+     return (
+          <div className="mb-16">
+               <div className="flex items-center flex-col justify-center mt-5 min-h-3/4">
+                    {/* <img src='https://static.vecteezy.com/system/resources/thumbnails/017/745/125/small_2x/empty-shopping-cart-side-view-for-designing-various-shopping-promotions-png.png' alt='cart-image'
+             className=' w-20 h-30'
+            /> */}
+
+                    <img
+                         src="https://icons.veryicon.com/png/o/commerce-shopping/fine-edition-mall-icon/wishlist-1.png"
+                         alt="wishlist-icon"
+                         className=" w-44 h-44"
+                    />
+
+                    <p className="text-red-500 font-bold text-xl">
+                         No items added yet? <span className="text-black">Continue shopping to explore more.</span>
+                    </p>
+                    <Link to="/login">
+                        
+                         <button className="bg-blue-600 text-white hover:bg-blue-800 duration-300 px-6  py-2 rounded-2xl  w-[300px] mt-9">
+                              Sign in
+                         </button>
+                    </Link>
+                    <Link to={"/shop"}>
+                        
+                         <button className="bg-blue-400 text-white hover:bg-blue-500 duration-300 px-6  py-2 rounded-2xl  w-[300px] mt-4">
+                              Explore items
+                         </button>
+                    </Link>
+               </div>
+               <ArtCard arts={arts} headline={"Explore more"}/>
+          </div>
+     );
+};
+
+export default EmptyWishlist;
