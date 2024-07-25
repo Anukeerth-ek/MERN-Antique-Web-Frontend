44ca400a6170d01710e72798319875ddd2d936a1
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-07-25 11:32:53 +0530

    Fixed bug in EmptyComponent

--- src/components/WishList.jsx
+++ src/components/WishList.jsx
@@ -1,16 +1,21 @@
 import React, { useContext } from "react";
 import { WishlistContext } from "../contexts/WishlistContext";
 import ArtCard from "./ArtCard";
-import EmptyWishlist from "./EmptyWishlist";
+import EmptyWishlist from "./EmptyComponent";
+import emptyWishlistImage from '../assets/wishlist-1.png'
 
 const WishList = () => {
      const { wishlist } = useContext(WishlistContext);
 
      return (
           <div>
-               {wishlist.length > 0 ?  <div className="mt-2">
-                    <ArtCard arts={wishlist} headline={"Wishlist"} /> 
-               </div> : <EmptyWishlist/> }
+               {wishlist.length > 0 ? (
+                    <div className="mt-2">
+                         <ArtCard arts={wishlist} headline={"Wishlist"} />
+                    </div>
+               ) : (
+                    <EmptyWishlist image={emptyWishlistImage}/>
+               )}
           </div>
      );
 };
