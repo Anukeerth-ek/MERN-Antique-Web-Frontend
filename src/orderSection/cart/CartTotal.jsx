7f8d95a4d3c0bca2ff710138ade5ec07f06d161b
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-07-27 01:04:22 +0530

    Fixed bug in CartTotal

--- src/orderSection/cart/CartTotal.jsx
+++ src/orderSection/cart/CartTotal.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { FaLongArrowAltRight } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const CartTotal = ({totalCartPrice}) => {
-    console.log(totalCartPrice)
+  
+    const navigate = useNavigate()
      return (
           <div className="flex justify-between mt-10 ">
                <div className="w-1/2"></div>
@@ -20,7 +22,7 @@ const CartTotal = ({totalCartPrice}) => {
                     <strong>Total Price</strong>
                     <strong>${totalCartPrice}</strong>
                 </div>
-               <button className="w-full flex items-center justify-center bg-blue-500 hover:bg-blue-800 duration-300 mt-10 text-white py-[10px] rounded-md">Proceed to Checkout <FaLongArrowAltRight className="ml-2"/></button>
+               <button className="w-full flex items-center justify-center bg-blue-500 hover:bg-blue-800 duration-300 mt-10 text-white py-[10px] rounded-md" onClick={()=> navigate('/checkout')}>Proceed to Checkout <FaLongArrowAltRight className="ml-2"/></button>
                </div>
           </div>
      );
